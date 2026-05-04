5cebb930399521a4a7aeda30cfaeb418df9c6530
Jeffrey Zonkel <jeffreyzonkel@gmail.com>
2026-05-04 00:06:32 +0000

    File changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -87,6 +87,15 @@ export default function Contact() {
                   </Button>
                 </form>
               </div>
+
+              {/* Monkey */}
+              <div className="mt-8 rounded-2xl overflow-hidden">
+                <img
+                  src="https://media.base44.com/images/public/69f7b435c4e1fadd6b3c10d5/14b5add35_monkey.jpg"
+                  alt="PANACEA mascot"
+                  className="w-full object-cover rounded-2xl"
+                />
+              </div>
             </motion.div>
 
             {/* Info + Map */}
